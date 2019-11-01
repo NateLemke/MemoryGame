8b3e9061c8fb068334ee00f387e82c2578db812e
Nate Lemke <nlemke@my.bcit.ca>
2019-10-31 17:51:54 -0700

    Finished resizing grid

--- public/js/game.js
+++ public/js/game.js
@@ -2,6 +2,8 @@
 var correctSound = new Audio("sounds/yea.mp3");
 //plays when container rotates
 var shuffleSound = new Audio("sounds/shuffle.mp3");
+//plays when the player make 4 wrong answers on 1 pattern
+var failSound = new Audio("sounds/boo.mp3");
 //the player's score
 var p_score = 10;
 //chooses the direction the container rotates
@@ -14,41 +16,60 @@ var deg = 0;
 var difficulty = 5;
 //the number of correct tiles the player has picked for the current pattern
 var picked = 0;
-//the number of correct tiles in the current pattern
-var corrects = 0;
+//the current width of the grid
+var gridWidth = 5;
+//the current height of the grid
+var gridHeight = 5;
+//The number of wrong answers the player has picked for this pattern
+var wrongAnswers = 0;
 
 //an array showing which tiles are correct
 var tileArray = [];
 
 
-//resets tile array and fills it with 0s
+//resets tile array and fills it with 0s. resizes the grid.
 function zeroArray() {
-    for(let i=0; i<5; i++) {
+    var main = document.getElementById("main_container");
+     main.innerHTML = '';
+
+    var startButton = document.createElement("button");
+    var restartButton = document.createElement('button');
+    startButton.setAttribute("id", "startButton");
+    startButton.setAttribute("onclick", "hideStart()");
+    restartButton.setAttribute("id", "restartButton");
+    restartButton.setAttribute("onclick", "restart()");
+
+    for(let i=0; i<gridWidth; i++) {
         tileArray[i] = [];
     }
-    for(let i=0; i<5; i++) {
-        for(let j=0; j<5; j++) {
+    for(let i=0; i<gridWidth; i++) {
+        for(let j=0; j<gridHeight; j++) {
             tileArray[i][j] = 0;
-        }
-    }
 
-    var tiles = document.getElementsByClassName('panel');
-    
-    for(let i=0; i<tiles.length; i++) {
-        tiles[i].setAttribute( "onclick", "clickTile(this.id)");
+            var t = document.createElement("div");
+            t.setAttribute("class", "tile");
+            t.setAttribute("id", i+","+j);
+            t.setAttribute("onclick", "clickTile(this.id)");
+
+            main.appendChild(t);
+        }
+        br = document.createElement("br");
+        main.appendChild(br);
     }
 }
 
 //hides the start button
 function hideStart() {
     document.getElementById("startButton").style.display="none";
-    showTiles();
     startGame();
 }
 
 //starts the game. Displays tiles and their initial pattern.
 function startGame() {
+    picked = 0;
+    wrongAnswers = 0;
     zeroArray();
+    showTiles();
     setPattern();
     showPattern();
     window.setTimeout(continueGame, 1000);
@@ -92,19 +113,36 @@ function clickTile(id) {
         p_score++;
         document.getElementById(xy).style.backgroundColor = "green";
         document.getElementById(xy).onclick = '';
-        if(picked >= corrects) {
-            picked = 0;
+        if(picked >= difficulty) {
             correctSound.play();
+            if(wrongAnswers <= 1 && (gridWidth <= 8 || gridHeight <= 8)) {
+                if(gridWidth <= gridHeight) {
+                    gridWidth++;
+                } else {
+                    gridHeight++;
+                }
+            }
             window.setTimeout(hidePattern, 900);
             window.setTimeout(startGame, 1000);
         }
     } else {
         p_score--;
+        wrongAnswers++;
         document.getElementById(xy).style.backgroundColor = "red";
         document.getElementById(xy).onclick = '';
+        if(wrongAnswers == 3 && (gridWidth > 5 || gridHeight > 5)) {
+            if(gridWidth >= gridHeight) {
+                gridWidth--;
+            } else {
+                gridHeight--;
+            }
+        }
+        if(wrongAnswers > 3) {
+            failSound.play();
+            window.setTimeout(hidePattern, 1000);
+            window.setTimeout(startGame, 1500);
+        }
         if(p_score <= 0) {
-            var mc = document.getElementById("main_container");
-            mc.style.transform = "rotate(0deg)";
             hideTiles();
             document.getElementById("restartButton").style.display = "inline-block";
         }
@@ -119,16 +157,14 @@ function setPattern() {
     //zeroArray();
 
     if(difficulty < 12) {
-        difficulty = 4 + (p_score/10);
+        difficulty = 4 + (p_score/20);
     }
-
-    corrects = difficulty;
     
     let tiles = 0;
 
     while(tiles < difficulty) {
-        let x = Math.round(Math.random() * 4);
-        let y = Math.round(Math.random() * 4);
+        let x = Math.round(Math.random() * (gridWidth - 1));
+        let y = Math.round(Math.random() * (gridHeight - 1));
 
         if(tileArray[x][y] == 0) {
             tileArray[x][y] = 1;
@@ -140,7 +176,7 @@ function setPattern() {
 
 //hides all tiles
 function hideTiles() {
-    var tiles = document.getElementsByClassName('panel');
+    var tiles = document.getElementsByClassName('tile');
     
     for(let i=0; i<tiles.length; i++) {
         tiles[i].style.display="none";
@@ -149,7 +185,7 @@ function hideTiles() {
 
 //shows all tiles
 function showTiles() {
-    var tiles = document.getElementsByClassName('panel');
+    var tiles = document.getElementsByClassName('tile');
     
     for(let i=0; i<tiles.length; i++) {
         tiles[i].style.display="inline-block";
@@ -158,7 +194,7 @@ function showTiles() {
 
 //shows the pattern.
 function showPattern() {
-    var tiles = document.getElementsByClassName('panel');
+    var tiles = document.getElementsByClassName('tile');
     
     for(let i=0; i<tiles.length; i++) {
         var xy = tiles[i].id.split(",");
@@ -171,7 +207,7 @@ function showPattern() {
 
 //hides the pattern.
 function hidePattern() {
-    var tiles = document.getElementsByClassName('panel');
+    var tiles = document.getElementsByClassName('tile');
     
     for(let i=0; i<tiles.length; i++) {
             tiles[i].style.backgroundColor="lightpink";
